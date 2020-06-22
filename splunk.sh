395d5c2009ceb7625d1dec6e2e17f301de85c786
ashwini <53709167+ashwini1331@users.noreply.github.com>
2020-06-22 18:40:03 +0530

    Update splunk.sh

--- splunk.sh
+++ splunk.sh
@@ -5,21 +5,4 @@
      sudo /opt/splunk/bin/splunk start --accept-license
      useradd -p 12345678,12345678 admin
     
- #!bin/bash
-     if [ $(id -u) -eq 0 ]; then
-	read -p "administrator username : " admin
-	read -s -p "enter password : " 12345678
-	read -s -p "Renter password : "12345678
-	egrep "^$username" /etc/passwd >/dev/null
-	if [ $? -eq 0 ]; then
-		echo "$username exists!"
-		exit 1
-	else
-		pass=$(perl -e 'print crypt($ARGV[0], "password")' $password)
-		useradd -m -p "$pass" "$username"
-		[ $? -eq 0 ] && echo "User has been added to system!" || echo "Failed to add a user!"
-	fi
-else
-	echo "Only root may add a user to the system."
-	exit 2
-fi
+ 
