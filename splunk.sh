976b72591c8239dd2058639476cc750b97d97c3e
ashwini <53709167+ashwini1331@users.noreply.github.com>
2020-06-22 18:05:42 +0530

    Update splunk.sh

--- splunk.sh
+++ splunk.sh
@@ -1,4 +1,5 @@
-      cd /opt/
-      tar xvzf splunk-8.0.4.1-ab7a85abaa98-Linux-x86_64.tgz
-      cd ../
-      sudo /opt/splunk/bin/splunk start --accept-license
+     sudo su 
+     cd /opt/
+     tar xvzf splunk-8.0.4.1-ab7a85abaa98-Linux-x86_64.tgz
+     cd ../
+     sudo /opt/splunk/bin/splunk start --accept-license
