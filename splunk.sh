b86dadd8a6913bc82112ed91d31270012b32b833
ashwini <53709167+ashwini1331@users.noreply.github.com>
2020-06-23 13:00:03 +0530

    Update splunk.sh

--- splunk.sh
+++ splunk.sh
@@ -1,3 +1,5 @@
+     wget -O splunk-8.0.4.1-ab7a85abaa98-Linux-x86_64.tgz 'https://www.splunk.com/bin/splunk/DownloadActivityServlet?architecture=x86_64&platform=linux&version=8.0.4.1&product=splunk&filename=splunk-8.0.4.1-ab7a85abaa98-Linux-x86_64.tgz&wget=true'
+     sudo mv splunk-8.0.4.1-ab7a85abaa98-Linux-x86_64.tgz /opt/
      sudo su 
      cd /opt/
      tar xvzf splunk-8.0.4.1-ab7a85abaa98-Linux-x86_64.tgz
