ba0a0bc92401cbf91c39ac92bb5a13a7c96b06c9
ashwini <53709167+ashwini1331@users.noreply.github.com>
2020-06-23 13:23:07 +0530

    Update splunk.sh

--- splunk.sh
+++ splunk.sh
@@ -4,7 +4,7 @@
      cd /opt/
      tar xvzf splunk-8.0.4.1-ab7a85abaa98-Linux-x86_64.tgz
      cd ../
-     sudo /opt/splunk/bin/splunk start --accept-license --answer-yes --no-prompt --seed-passwd 12345678
+     sudo /opt/splunk/bin/splunk start --accept-license --answer-yes --no-prompt --gen-and-print-passwd
      
      
     
